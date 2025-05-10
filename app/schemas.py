148f7eba2cc9c3711dd72d9c0b648f89f28ef4ef
Ana <akarinavillagrani@gmail.com>
2025-05-10 00:01:03 -0400

    init project

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, ConfigDict
+
+class ItemBase(BaseModel):
+    name: str
+    description: str
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
